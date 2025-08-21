6394866927352266dfcc7677387b0f4911de0397
Itay Katav <ikatav@redhat.com>
2025-08-21 10:17:43 +0300

    feat: migrate to sqlmodel
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- src/alm/database.py
+++ src/alm/database.py
@@ -1,20 +1,30 @@
 from __future__ import annotations
 
+import asyncio
 import os
 from typing import Generator
 
-from sqlmodel import Session, create_engine
-
-# Database configuration
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://user:password@localhost:5432/logsdb"
-)
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.alm.models import GrafanaAlert
 
 # Create SQLModel engine
-engine = create_engine(DATABASE_URL)
+engine = create_async_engine(os.getenv("DATABASE_URL"))
 
 
-# Dependency to get database session
-def get_db() -> Generator[Session, None, None]:
-    with Session(engine) as session:
+# Create tables
+async def init_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(GrafanaAlert.metadata.drop_all)
+        await conn.run_sync(GrafanaAlert.metadata.create_all)
+
+
+def get_session():
+    session = AsyncSession(engine)
+    return session
+
+
+async def get_session_gen() -> Generator[AsyncSession, None, None]:
+    async with get_session() as session:
         yield session
