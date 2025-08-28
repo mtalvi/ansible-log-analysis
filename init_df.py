4dfff6f7842e37c84312598555d0adb63bdf09ec
Itay Katav <ikatav@redhat.com>
2025-08-28 17:23:31 +0300

    feat: do whole initialization
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- init_df.py
+++ init_df.py
@@ -4,12 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from alm.pipeline.find_categories import only_generate_step_by_step_solutions
+from alm.pipeline.find_categories import (
+    only_generate_step_by_step_solutions,
+    whole_pipeline,
+)
 
 
 async def main():
     print(os.getenv("DATABASE_URL"))
-    await only_generate_step_by_step_solutions()
+    await whole_pipeline()
 
 
 if __name__ == "__main__":
