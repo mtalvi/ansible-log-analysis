8e00394f9df752c27ac72ba72b57f15c01077678
Itay Katav <ikatav@redhat.com>
2025-10-23 13:07:42 +0300

    feat: add sub agent get more context
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- src/alm/agents/get_more_context_agent/graph.py
+++ src/alm/agents/get_more_context_agent/graph.py
@@ -1,22 +1,48 @@
 from src.alm.llm import get_llm
 
-# from src.alm.agents.get_more_context_agent.node import (
-#     get_more_context,
-# )
-# from langgraph.graph import StateGraph, START, END
-# from langgraph.types import Command
-
-# from typing import Literal
+from src.alm.agents.get_more_context_agent.node import (
+    get_cheat_sheet_context,
+    loki_router,
+)
+from src.alm.agents.get_more_context_agent.state import ContextAgentState
+from langgraph.graph import StateGraph, START, END
+from langgraph.types import Command
 
 llm = get_llm()
 
 
+async def cheat_sheet_context_node(state: ContextAgentState):
+    cheat_sheet_context = await get_cheat_sheet_context(state.log_summary)
+    return Command(
+        goto="loki_router_node", update={"cheat_sheet_context": cheat_sheet_context}
+    )
+
+
+async def loki_router_node(state: ContextAgentState):
+    reasoning, classification = await loki_router(
+        state.log_summary, state.cheat_sheet_context, llm
+    )
+    return Command(
+        goto="loki_sub_agent"
+        if classification == "need_more_context_from_loki_db"
+        else END,
+        update={"reasoning": reasoning, "classification": classification},
+    )
+
+
+async def loki_sub_agent(state: ContextAgentState):
+    # context = await loki_agent.ainvoke({}) # TODO choose the input for the agent NOTE you have reasoning why we need loki, can be used or removed.
+    context = None  # TODO change me
+    return Command(goto=END, update={"loki_context": context})
+
+
 def build_graph():
-    pass  # TODO
+    builder = StateGraph(ContextAgentState)
+    builder.add_edge(START, "cheat_sheet_context_node")
+    builder.add_node("cheat_sheet_context_node", cheat_sheet_context_node)
+    builder.add_node("loki_router_node", loki_router_node)
+    builder.add_node("loki_sub_agent", loki_sub_agent)
+    return builder.compile()
 
 
-_graph = build_graph()
-
-
-def get_graph():
-    return _graph
+more_context_agent_graph = build_graph()
---
+++ src/alm/agents/get_more_context_agent/node.py
@@ -0,0 +1,42 @@
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+class LokiRouterSchema(BaseModel):
+    reasoning: str = Field(description="the reasoning for the decision")
+    classification: Literal[
+        "need_more_context_from_loki_db", "no_need_more_context_from_loki_db"
+    ] = Field(
+        description="determines if we need to fetch more context from loki db, 'need_more_context_from_loki_db' if we need to fetch more context, 'no_need_more_context_from_loki_db' if we don't need to fetch more context"
+    )
+
+
+with open("src/alm/agents/get_more_context_agent/prompts/loki_router.md", "r") as f:
+    loki_router_user_message = f.read()
+
+
+async def get_cheat_sheet_context(log_summary: str):
+    # retrive context that is relevent to solve the problem
+    # context = TODO
+    # return context
+    return ""
+
+
+async def loki_router(log_summary: str, cheat_sheet_context: str, llm: ChatOpenAI):
+    llm_structured = llm.with_structured_output(LokiRouterSchema)
+    output = await llm_structured.ainvoke(
+        [
+            {
+                "role": "system",
+                "content": "You are an Ansible expert and helpful assistant",
+            },
+            {
+                "role": "user",
+                "content": loki_router_user_message.format(
+                    log_summary=log_summary, cheat_sheet_context=cheat_sheet_context
+                ),
+            },
+        ]
+    )
+    return output.reasoning, output.classification
